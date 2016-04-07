f949c49fc886674cb01af564a2f110856a9b38a1
Luka Klacar <l.klacar@vegaitsourcing.rs>
2016-04-07 16:37:32 +0200

    Added virtual dom example

--- src/app.js
+++ src/app.js
@@ -1,58 +1,35 @@
-import $ from "jquery"
-
-class Template {
-
-    constructor() {
-        this.handlers = {
-            "data-bind": dataBind,
-        }
-    }
-
-    render(selector, html, data) {
-
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(html, "text/html");
-        this.traverse(doc, data);
-        $(selector).html(doc.innerHTML);
-
-    }
-
-    traverse(element, data) {
-
-        this.invokeHandler(element, data);
-        for (var i = 0; i < element.children.length; i++)
-            this.traverse(element.children[i], data);
-    }
-
-    invokeHandler(element, data) {
-        Object.keys(this.handlers).forEach(function (key) {
-
-            if (element instanceof HTMLElement && element.hasAttribute(key))
-                this.handlers[key](element, data, key);
-
-        }.bind(this));
-    }
-
-}
-
-
-function dataBind(element, data, attribute) {
-
-    var bindDefinition = element.getAttribute(attribute);
-
-
-
-
-    
-
-
-}
-
+var $ = require('jquery');
 
 $(document).ready(function () {
-    var t = new Template();
 
-    t.render('body', require("./template.html"), {});
+    var h = require('virtual-dom/h');
+    var diff = require('virtual-dom/diff');
+    var patch = require('virtual-dom/patch');
+    var createElement = require('virtual-dom/create-element');
+
+
+    function render(count) {
+        return h("div", ["\n", h("h1", [count]), "\n", h("h1", ["asd"]), "\n", h("input", {"attributes": {"type": "text"}})])
+    }
+
+
+    var count = 0;
+
+    var tree = render(count);
+    var rootNode = createElement(tree);
+    document.body.appendChild(rootNode);
+
+
+    setInterval(function () {
+        count++;
+
+        var newTree = render(count);
+        var patches = diff(tree, newTree);
+        console.log(patches);
+
+        rootNode = patch(rootNode, patches);
+        tree = newTree;
+    }, 1000);
+
 
 });
-
